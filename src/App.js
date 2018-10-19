0fb8323f3a82e63259318f0744f8a6c36eaa41c7
Golam Rabbani <knight3rrantt@gmail.com>
2018-10-19 18:24:38 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Relayr Device Dashboard</h1>
+            <p>Feel free to implement UI the way you like.</p>
+            <p>You can write styles in ./styles.css.</p>
+        </div>
+    );
+}
+
+export default App;
